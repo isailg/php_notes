08a4b39d92618a0a531f5505f14aaa61e49b61a8
isailg <ilogamx@gmail.com>
2020-08-11 18:58:26 -0500

    OOP Interfaces

---
+++ app/Models/BaseElement.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'app/Models/Printable.php';
+
+class BaseElement implements Printable {
+  private $title;
+  public $description;
+  public $visible= true;
+  public $months;
+
+  public function __construct($tit, $des) {
+    $this->setTitle($tit);
+    $this->description= $des;
+  }
+
+  public function setTitle($t){
+    if($t == ''){
+      $this->title = 'N/A';
+    }else {
+      $this->title =$t;
+    }
+  }
+
+  public function getTitle(){
+    return $this->title;
+  }
+
+  public function getDurationAsString(){
+    $years = floor ($this->months / 12);
+    $extraMonths = $this->months % 12;
+
+    return "$years years $extraMonths months";
+  }
+  public function getDescription(){
+    return $this->description;
+  }
+}
---
+++ jobs.php
@@ -0,0 +1,79 @@
+<?php
+require 'app/Models/Job.php';
+require 'app/Models/Project.php';
+
+$job1 = new Job('PHP Developer','This is for real!!!');
+$job1->months = 16;
+
+$job2 = new Job('Python developer','this is for OK!!!');
+$job2->months = 24;
+
+$job3 = new Job('','this is for real!!!');
+$job3->months = 24;
+
+$jobs = [
+  $job1,
+  $job2,
+  $job3
+];
+
+$project1 = new Project ('Project1','Description1');
+
+$projects = [
+  $project1 ];
+
+function printElement(Printable $job){
+  if ($job->visible == false){
+    return;
+  }
+  echo '<li class="work-position">';
+  echo '<h5>' . $job->getTitle() . '</h5>';
+  echo '<p>' . $job->getDescription() . ' </p>';
+  echo '<p>' . $job->getDurationAsString() . ' </p>';
+  echo '<strong>Achievements:</strong>';
+  echo '<ul>';
+  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '<li>Lorem ipsum dolor sit amet, 80% consectetuer adipiscing elit.</li>';
+  echo '</ul>';
+  echo '</li>';
+}
+
+
+
+
+
+
+
+/*Array
+$array = array (
+  "course1" => "php",
+  "course2" => "js",
+);
+
+$jobs= [
+  0 => 'PHP Developer',
+  1 => 'Python Developer',
+  2 => 'DevOps'
+];
+ //var_dump($jobs[2]);
+$jobs1 = [
+  [
+    'title' => 'PHP Developer',
+    'description' => 'Full Stack Developer',
+    'visible' => 'true'
+  ],
+  [
+    'title' => 'Python Developer'
+  ],
+  [
+    'title' => 'DevOps'
+  ],
+  [
+    'title' => 'Go Developer'
+  ],
+  [
+    'title' => 'Frontend Dev'
+  ]
+];
+*/
