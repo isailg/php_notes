d62487dac6bdce83bcfbd9ce71ab26c45d220c2a
isailg <ilogamx@gmail.com>
2020-10-30 13:06:50 -0600

    Files upload

--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -16,15 +16,23 @@ class JobsController extends BaseController{
            try {
                $jobValidator->assert($postData);
                $postData = $request->getParsedBody();
-               $job = new Job();
-               $job->title = $postData['title'];
-               $job->description = $postData['description'];
-               $job->save();
+
+               $files = $request->getUploadedFiles();
+                $logo = $files['logo'];
+
+                if($logo->getError() == UPLOAD_ERR_OK) {
+                    $fileName = $logo->getClientFilename();
+                    $logo->moveTo("uploads/$fileName");
+                }
+               // $job = new Job();
+               // $job->title = $postData['title'];
+               // $job->description = $postData['description'];
+               // $job->save();
 
                $responseMessage = 'Saved';
            } catch (\Exception $e) {
-               // $responseMessage = $e->getMessage();
-               $responseMessage = "Campos vacíos";
+               $responseMessage = $e->getMessage();
+               // $responseMessage = "Campos vacíos";
            }
        }
 
